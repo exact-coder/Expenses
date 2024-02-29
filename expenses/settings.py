1ef3694b3d0265428dba793c3b7ed8a03d157811
Programming Bees <hasanakash799@gmail.com>
2024-03-01 02:10:00 +0600

    feta: templates setup

--- expenses/settings.py
+++ expenses/settings.py
@@ -22,6 +22,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'home',
 ]
 
 MIDDLEWARE = [
@@ -39,7 +41,7 @@ ROOT_URLCONF = 'expenses.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -56,7 +58,6 @@ WSGI_APPLICATION = 'expenses.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 # DATABASES = {
 #     'default': {
@@ -112,6 +113,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT='static'
+STATICFILES_DIRS=[os.path.join(BASE_DIR,'expenses/static')]
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
---
+++ home/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from home.views import index,add_expense
+
+urlpatterns = [
+    path("",index,name="index"),
+    path("add-expense/",add_expense,name="add-expense"),
+]
