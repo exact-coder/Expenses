3ca0ef2b1695b82c7f931d189d479d5848e82ac8
Programming Bees <hasanakash799@gmail.com>
2024-03-07 21:27:03 +0600

    chore: added register page

---
+++ authentication/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from .views import RegistrationView
+
+urlpatterns = [
+    path("register",RegistrationView.as_view(),name="register")
+]
--- expenses/settings.py
+++ expenses/settings.py
@@ -24,6 +24,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'home',
+    'authentication',
+    
 ]
 
 MIDDLEWARE = [
