5c12ae6bd229252c1ccf10a579b07eef220ac4ed
Programming Bees <hasanakash799@gmail.com>
2024-03-11 22:21:48 +0600

    chore: added form in add_expenses

--- home/views.py
+++ home/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from home.models import Category
 
 # Create your views here.
 
 @login_required(login_url="login")
 def index(request):
+    categories = Category.objects.all()
     return render(request,"index.html")
 
 @login_required(login_url="login")
 def add_expense(request):
-    return render(request,"add_expense.html")
+    categories = Category.objects.all()
+    context={
+        'categories':categories,
+    }
+    return render(request,"add_expense.html",context)
