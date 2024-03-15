8493a6e410423f2f1a2e754e7c57647b51d1a8dd
Programming Bees <hasanakash799@gmail.com>
2024-03-16 01:18:35 +0600

    chore: added expenses data download in excel format

--- home/urls.py
+++ home/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from home.views import index,add_expense,expense_edit,delete_expense,search_expenses,expense_category_summary,stats_view,export_csv
+from home.views import index,add_expense,expense_edit,delete_expense,search_expenses,expense_category_summary,stats_view,export_csv,export_excel
 from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns = [
@@ -9,6 +9,7 @@ urlpatterns = [
     path("delete-expense/<int:id>",delete_expense,name="delete-expense"),
     path("stats",stats_view,name="stats"),
     path("export-csv",export_csv,name="export-csv"),
+    path("export-excel",export_excel,name="export-excel"),
     path("search-expenses",csrf_exempt(search_expenses),name="search_expenses"), # type: ignore
     path("expense_category_summary",csrf_exempt(expense_category_summary),name="expense_category_summary"), # type: ignore
 ]
--- home/views.py
+++ home/views.py
@@ -8,6 +8,7 @@ from django.http import JsonResponse,HttpResponse
 from userpreferences.models import UserPreference
 import datetime
 import csv
+import xlwt
 
 
 # Create your views here.
@@ -145,4 +146,27 @@ def export_csv(request):
 
     for expense in expenses:
         writer.writerow([expense.amount,expense.description,expense.category,expense.date])
+    return response
+
+def export_excel(request):
+    response = HttpResponse(content_type='application/ms-excel')
+    response['Content-Disposition'] = f'attachment; filename=Expenses-{datetime.datetime.now()}'+'.xls'
+    wb = xlwt.Workbook(encoding='utf-8')
+    ws = wb.add_sheet('Expenses')
+    row_num = 0
+    font_style=xlwt.XFStyle()
+    font_style.font.bold = True
+    
+    columns = ['Account','Description','Category','Date']
+    for col_num in range(len(columns)):
+        ws.write(row_num,col_num,columns[col_num],font_style)
+
+    font_style=xlwt.XFStyle()
+    rows = Expense.objects.filter(owner=request.user).values_list('amount','description','category','date')
+
+    for row in rows:
+        row_num+=1
+        for col_num in range(len(row)):
+            ws.write(row_num,col_num,str(row[col_num]),font_style)
+    wb.save(response)
     return response
