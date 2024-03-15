40fe9758491281de02b7b163f0aaa9836a7d44c1
Programming Bees <hasanakash799@gmail.com>
2024-03-15 23:43:49 +0600

    chore: chartjs shows expenses in doughnut formet

--- home/urls.py
+++ home/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from home.views import index,add_expense,expense_edit,delete_expense,search_expenses
+from home.views import index,add_expense,expense_edit,delete_expense,search_expenses,expense_category_summary,stats_view
 from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns = [
@@ -7,5 +7,7 @@ urlpatterns = [
     path("add-expense/",add_expense,name="add-expense"),
     path("expense-edit/<int:id>",expense_edit,name="expense-edit"),
     path("delete-expense/<int:id>",delete_expense,name="delete-expense"),
+    path("stats",stats_view,name="stats"),
     path("search-expenses",csrf_exempt(search_expenses),name="search_expenses"), # type: ignore
+    path("expense_category_summary",csrf_exempt(expense_category_summary),name="expense_category_summary"), # type: ignore
 ]
